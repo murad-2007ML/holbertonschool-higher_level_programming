4e2a42be28e053b4ece91e0a59caef74185265e6
root <haliqhuseynov2000@gmail.com>
2025-11-25 13:57:47 +0000

    ajsja

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
+# Mənfi ədədlər üçün son rəqəmin işarəsini düzəldirik
+# Əgər ədəd mənfidirsə VƏ qalıq sıfır deyilsə, 10 çıxarırıq.
 if last_digit > 0 and number < 0:
     last_digit -= 10
 if last_digit > 5:
@@ -9,6 +11,8 @@ if last_digit > 5:
 elif last_digit == 0:
     result = "and is 0"
 else:
+    # Bu şərt (6-dan kiçik və 0-a bərabər olmayan) bütün digər ehtimalları əhatə edir:
+    # 1-dən 5-ə qədər müsbət rəqəmlər və -1-dən -9-a qədər mənfi rəqəmlər.
     result = "and is less than 6 and not 0"
 print(f"Last digit of {number} is {last_digit} {result}")
 ~                                                                                                                                                                                                                                        
