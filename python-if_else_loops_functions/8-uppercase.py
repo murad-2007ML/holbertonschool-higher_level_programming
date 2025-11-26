427a7a5233ffa614cf9937d728db8e13baac75a6
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-11-26 12:37:36 +0000

    ahah

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str1=''
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            str1 += chr(ord(i) - 32)
+        else:
+            str1 += i
+    return str1
