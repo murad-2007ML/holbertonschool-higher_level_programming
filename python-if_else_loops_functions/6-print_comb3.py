75d2a34e2ac9267993576eda37f10a57d3158538
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-11-26 11:50:55 +0000

    asba

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 10):
     for h in range(i + 1, 10):
         if i == 8 and h == 9:
             print("{:02d}".format(i * 10 + h))
-        else: 
+        else:
             print("{:02d}".format(i * 10 + h), end=", ")
