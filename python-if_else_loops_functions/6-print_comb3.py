af8268ae458fafcfcfc9c50f7ca644307f2845f8
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-11-26 11:48:13 +0000

    hbsh

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for h in range(i + 1, 10):
+        if i == 8 and h == 9:
+            print("{:02d}".format(i * 10 + h))
+        else: 
+            print("{:02d}".format(i * 10 + h), end=", ")
