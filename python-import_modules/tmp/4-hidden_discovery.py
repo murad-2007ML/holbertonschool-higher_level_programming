e8881622d0747ca06fb10c6f3cf401660a0681aa
sdf <sdf>
2025-12-01 14:50:24 +0000

    d

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 
-import sys
-import hidden_4
+    import hidden_4
 
-for i in dir(hidden_4):
-    if i[0] != "_":
-        print(i)
+    for i in dir(hidden_4):
+        if (not i.startswith("__")):
+            print(i)
