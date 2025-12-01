e7d8b59eb1fec5ebf8a6112af5a61b862a047a8d
sdf <sdf>
2025-12-01 14:48:11 +0000

    d

--- python-import_modules/4-hidden-discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import sys
-import hidden_4
-
-for i in dir(hidden_4):
-    if i[0] != "_":
-        print(i)
--- tmp/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import hidden_4
-import sys
-
-for i in dir(hidden_4):
-    if i[0] != "_":
-         print(i)
