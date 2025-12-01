82a12218c8c9fedf7fa67ce9fad965b4bb710330
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-01 13:02:05 +0000

    python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         for i in range(1, len(sys.argv)):
+            print("{:d} argument:".format(i))
             print("{:d} = {}".format(i, sys.argv[i]))
