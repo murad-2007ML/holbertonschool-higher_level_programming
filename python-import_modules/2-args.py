51b9464536407fb619ab8cb02cf962a868eb3957
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-01 13:14:38 +0000

    python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         if len(sys.argv) == 2:
             print("1 argument:")
         else:
-            print("{:d}".format(len(sys.argv)))
-        for i in range(1, len(sys.argv))
+            print("{:d}".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
             print("{:d}: {}".format(i, sys.argv[i]))
