f8b479cc3db715dcd1fedf6f08d71616b7854b47
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-01 11:50:54 +0000

    asbhdsa

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+from add_0.py import add
+def holbie():    
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__holbie__":
+    holbie()
