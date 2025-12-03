02570850ae06b3e2a9d7a99d087be3e023ce9cf8
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-03 16:18:31 +0000

    python

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import calculator_1 as calc
 import sys
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     b = int(sys.argv[3])
     if operator == "+":
-        result = add(a, b)
+        result = calc.add(a, b)
     elif operator == "-":
-        result = sub(a, b)
+        result = calc.sub(a, b)
     elif operator == "*":
-        result = mul(a, b)
+        result = calc.mul(a, b)
     elif operator == "/":
-        result = div(a, b)
+        result = calc.div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
