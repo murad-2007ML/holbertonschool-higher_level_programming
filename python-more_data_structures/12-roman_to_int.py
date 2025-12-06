110430fb7c0e42937bbd7c14ae35ac0c696e13d3
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-06 13:36:34 +0000

    ashdb

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,13 @@ def roman_to_int(roman_string):
     a = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     b = 0
     s = 0
-    for i in reversed(roman_string):
-        if a[i] < b:
-            s -= a[i]
-        else:
-            s += a[i]
-        b = a[i]
+    if not roman_string:
+        return 0
+    else:
+        for i in reversed(roman_string):
+            if a[i] < b:
+                s -= a[i]
+            else:
+                s += a[i]
+            b = a[i]
     return s
