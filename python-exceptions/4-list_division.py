03243a72eb6d6a17101ac0d6794fd03e04449d48
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-06 18:26:30 +0000

    python

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     a = []
     for i in range(list_length):
         try:
-             result = my_list_1[i] / my_list_2[i]
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             result = 0
@@ -16,3 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             a.append(result)
     return a
+
