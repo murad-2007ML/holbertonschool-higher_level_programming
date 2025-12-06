9789529e92697bcc016401bc8ef88807ee9866f1
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-06 17:20:20 +0000

    ashda

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     a = 0
-    for i in range(a):
+    for i in range(x):
         try:
             print(my_list[i], end = "")
             a += 1
