6e3d75ef1162b0c148f47ef1b19f5900594377ef
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-06 13:47:40 +0000

    hasd

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    result, n = 0, 0
+    for i in my_list:
+        h = 1
+        n += i[1]
+        for j in i:
+            h = h * j
+        result += h
+    print("Average: {:0.2f}".format(result/n))
