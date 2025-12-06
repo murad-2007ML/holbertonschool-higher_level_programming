6104095ed7558dcd0dc4cb85694ebe946c1644cc
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-06 17:39:27 +0000

    python

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if len(value) >= 0:
-            print("{} is not an integer".format(value))
-            return False
-    except:
         print("{:d}".format(value))
         return True
+    except:
+        return False
