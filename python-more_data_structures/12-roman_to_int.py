ac7ac124c3faf9f4b091a8c53f92d3facd0bb097
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-06 13:39:31 +0000

    sahd

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     a = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     b = 0
     s = 0
-    if not roman_string:
+    if type(roman_string) is not str or not roman_string:
         return 0
     else:
         for i in reversed(roman_string):
