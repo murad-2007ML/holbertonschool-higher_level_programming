a243611618e91209d18be9807bc1010dee0bd48e
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-09 19:00:16 +0000

    python

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Defines a Rectangle"""
-    Rectangle.number_of_instances = 0
+    number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
