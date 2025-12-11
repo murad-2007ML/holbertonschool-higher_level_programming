e33d9e2e8b159ff84f3ca0954c017e4b557b4c87
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-11 14:59:57 +0000

    python

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,7 +5,7 @@
 class BaseGeometry:
     """Create an empty class"""
     def area(self):
-        raise Exceptation("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         self.value = value
