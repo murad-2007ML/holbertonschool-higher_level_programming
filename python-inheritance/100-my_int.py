91c424d3c0bcddfa1650e5ef33525a5b9a1e5485
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-11 16:21:39 +0000

    python

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Myint rebels"""
+
+def MyInt(int):
+    """MyInt rebel"""
+    def __eq__(self, other):
+        return super().__ne__(other)
+    def __ne__(self, other):
+        return super().__eq__(other)
