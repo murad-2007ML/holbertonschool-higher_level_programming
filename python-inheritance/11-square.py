a8fdabe539d8759947c924b20207ef9e15c21de1
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-11 15:45:15 +0000

    python

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -11,6 +11,6 @@ class Square(Rectangle):
 
     def area(self):
         return self.__size * self.__size
-    
+
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
