f36c50b08304ce58cf4343c838f67ab6288984f6
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-11 14:42:37 +0000

    python

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """It finds out it's directly inherited or not"""
-    if type(obj) is a_class or isinstance(obj, a_class):
+    if type(obj) is not a_class and isinstance(obj, a_class):
         return True
     return False
