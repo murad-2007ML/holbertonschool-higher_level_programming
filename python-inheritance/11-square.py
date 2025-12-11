c6bf4692e02584f3f07ed8a606a223e8a332c5f4
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-11 15:44:17 +0000

    python

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Inhertitance from Rectangle"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Inhertitance from Rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+    
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
