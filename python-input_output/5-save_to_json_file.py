5718aee4116eb2892c43d241082e455b0494c63a
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-14 09:53:05 +0000

    python

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""using JSON represantion"""
+import json as j
+
+
+def save_to_json_file(my_obj, filename):
+    """JSON represantion"""
+    with open(filename, encoding="utf-8") as f:
+        json.dump(my_obj, f)
