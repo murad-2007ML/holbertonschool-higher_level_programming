2c850d029802fe3ad9927f00b1bf408d32aba867
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-14 14:01:26 +0000

    python

---
+++ python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """class of Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (attrs.__class__ is list and all(a.__class__ is str for a in attrs)):
+            return {i: getattr(self, k) for i in attrs if hasattr(self, k)}:
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for a, b in json.items():
+            setattr(self, a, b)
