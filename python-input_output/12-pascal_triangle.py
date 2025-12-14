90f05192153bc8ee2dda5ad92ecd92cd99be9f88
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-14 14:26:05 +0000

    python

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    """pascal_triangle"""
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        a = [1]
+        if i >= 1:
+            b = triangle[i-1]
+            for k in range(1, i):
+                a.append(b[k] + b[k-1])
+            a.append(1)
+        triangle.append(a)
+    return triangle
+n = 5
+print(pascal_triangle(n))
