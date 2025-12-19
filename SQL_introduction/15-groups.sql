56a1b7355fdb9dcc6f122179a67979959ebf716a
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-19 09:34:26 +0000

    SQL

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- listing number of scores with same values
-SELECT score, name FROM second_table GROUP BY score ORDER BY count DESC; 
+SELECT score, count(score) AS number FROM second_table GROUP BY score ORDER BY number DESC; 
