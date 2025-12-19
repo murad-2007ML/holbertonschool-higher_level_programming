a2049574483f086a6b04f15655e5099299270f02
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-19 16:37:57 +0000

    SQL

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,3 @@
 -- listing all cities in database
-SELECT id, name FROM states WHERE state_id (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT id, name FROM states
+WHERE state_id (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
