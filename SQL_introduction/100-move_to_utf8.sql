8cfd7f9095a18b3c32604b79bbf3acebd58a34b3
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-19 09:48:34 +0000

    SQL

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- convertin database to UTF-8
+ALTER hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) SET utf8mb4 COLLATE utf8mb4_unicode_ci;
