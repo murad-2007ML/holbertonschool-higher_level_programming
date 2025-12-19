8bee5e69459d2ad35bd658bd48678d6f0448b6d1
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-19 10:22:34 +0000

    SQL

--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- average temperature0
-SELECT state, MAX(value) AS max_value FROM temperatures GROUP BY state ORDER BY max_value DESC;
+SELECT state, MAX(value) AS max_value FROM temperatures GROUP BY state ORDER BY state;
