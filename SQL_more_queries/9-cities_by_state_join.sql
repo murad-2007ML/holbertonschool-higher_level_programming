c75904446cee01dc46d4117efa8642e354bbd258
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-20 09:26:15 +0000

    SQL

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities in database
-SELECT cities,id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
