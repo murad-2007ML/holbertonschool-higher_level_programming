01fca3dcf867ec585c5392174fee944fe648f215
murad-2007ML <haliqhuseynov2000@gmail.com>
2025-12-20 12:24:28 +0000

    SQL

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_show.id = tv_show_genres.show_id
+ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_show.title ASC, tv_genres.name ASC;
